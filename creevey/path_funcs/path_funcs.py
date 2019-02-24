f5bee6e6ee82afed561c404bcfbaab6c02532805
Greg Gandenberger <ggandenberger@shoprunner.com>
2019-02-23 21:07:39 -0600

    Fix function name and add docstring

--- creevey/path_funcs/path_funcs.py
+++ creevey/path_funcs/path_funcs.py
@@ -1,8 +1,29 @@
+from creevey.constants import PathOrStr
+
 from pathlib import Path
 
 
-def combine_outdir_dirname_extension(url, outdir, extension):
-    filename = Path(Path(url).name)
+def combine_outdir_basename_extension(
+    path: PathOrStr, outdir: PathOrStr, extension: str
+) -> Path:
+    """
+    Join `outdir`, `path` filename, and `extension`
+
+    Parameters
+    ----------
+    path
+        Path with desired filename
+    outdir
+        Desired output directory
+    extension
+        Desired output file extension
+
+    Returns
+    -------
+    Path object that results from joining input `outdir` with filename
+    from input `path` with input `extension`.
+    """
+    filename = Path(Path(path).name)
     extension = extension if extension.startswith('.') else '.' + extension
     filename_with_ext = filename.stem + extension
     outpath = Path(outdir) / filename_with_ext

30f88d6032dbbbcddef3b76675129421d50991da
gsganden <gsganden@gmail.com>
2019-06-18 09:22:20 -0500

    Add CI script

---
+++ .ci/test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# guarantee script exits with non-zero code
+# the first time any of these commands fail
+set -e
+
+pytest
+flake8 creevey tests
+black creevey tests --skip-string-normalization --check

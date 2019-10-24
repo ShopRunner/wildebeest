51bce1ecb56d2a791b697628cd33b8e0bd3b65ab
gsganden <gsganden@gmail.com>
2019-10-24 12:27:37 -0500

    Remove old commented-out code

--- tests/test_ops/test_helpers/test_report.py
+++ tests/test_ops/test_helpers/test_report.py
@@ -1,36 +1,6 @@
 from collections import defaultdict
-from functools import partial
 
-from creevey import CustomReportingPipeline
-from creevey.load_funcs.image import load_image_from_url
 from creevey.ops import get_report_output_decorator, report_output
-from creevey.ops.image import calculate_mean_brightness
-from creevey.path_funcs import join_outdir_filename_extension
-from creevey.write_funcs.image import write_image
-
-# @get_report_output_decorator(key='mean_brightness')
-# def report_mean_brightness(image):
-#     return calculate_mean_brightness(image)
-#
-#
-# report_brightness_pipeline = CustomReportingPipeline(
-#     load_func=load_image_from_url, ops=[report_mean_brightness], write_func=write_image
-# )
-#
-# image_filenames = ['2RsJ8EQ', '2TqoToT', '2VocS58', '2scKPIp', '2TsO6Pc', '2SCv0q7']
-# image_urls = [f'https://bit.ly/{filename}' for filename in image_filenames]
-#
-# keep_filename_png_in_cwd = partial(
-#     join_outdir_filename_extension, outdir='.', extension='.png'
-# )
-# print(
-#     report_brightness_pipeline.run(
-#         inpaths=image_urls,
-#         path_func=keep_filename_png_in_cwd,
-#         n_jobs=1,
-#         skip_existing=False,
-#     )
-# )
 
 
 def test_report_output():

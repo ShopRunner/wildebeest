9e013e70ea9d0f88dfe33a4ac3d9c4a8a511d1a8
gsganden@gmail.com <gsganden@gmail.com>
2020-07-29 14:31:04 -0500

    Test string input

--- tests/test_path_funcs/test_path_funcs.py
+++ tests/test_path_funcs/test_path_funcs.py
@@ -50,6 +50,24 @@ def test_join_outdir_hashed_path_extension():
     assert actual_outpath2 == actual_outpath
 
 
+def test_join_outdir_hashed_str_input():
+    inpath = './foo.png'
+    actual_outpath = join_outdir_hashed_path_extension(
+        inpath, outdir='bar', extension='jpg'
+    )
+    expected_pardir = Path('bar')
+    expected_extension = '.jpg'
+    assert actual_outpath.parent == expected_pardir
+    assert actual_outpath.suffix == expected_extension
+    assert actual_outpath.stem != 'foo'
+
+    # check consistency in repeated applications
+    actual_outpath2 = join_outdir_hashed_path_extension(
+        inpath, outdir='bar', extension='jpg'
+    )
+    assert actual_outpath2 == actual_outpath
+
+
 def test_join_outdir_hashed_path_no_extension():
     inpath = Path('./foo.png')
     actual_outpath = join_outdir_hashed_path_extension(inpath, outdir='bar')

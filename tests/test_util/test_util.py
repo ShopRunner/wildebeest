b009f32af9aa31cd4831b566a109e66afa42aea2
gsganden@gmail.com <gsganden@gmail.com>
2020-07-29 17:01:29 -0500

    Remove noqa

--- tests/test_util/test_util.py
+++ tests/test_util/test_util.py
@@ -2,13 +2,13 @@ from tests.conftest import TEMP_DATA_DIR
 from wildebeest.util import find_files_with_extensions
 
 
-def test_find_files_with_extensions_without_dot(generate_file_tree):  # noqa: F811
+def test_find_files_with_extensions_without_dot(generate_file_tree):
     txt_paths = find_files_with_extensions(search_dir=TEMP_DATA_DIR, extensions=['txt'])
     assert all(path.exists() for path in txt_paths)
     assert len(txt_paths) == 2
 
 
-def test_find_files_with_extensions_with_dot(generate_file_tree):  # noqa: F811
+def test_find_files_with_extensions_with_dot(generate_file_tree):
     pdf_paths = find_files_with_extensions(
         search_dir=TEMP_DATA_DIR, extensions=['.pdf']
     )
@@ -16,7 +16,7 @@ def test_find_files_with_extensions_with_dot(generate_file_tree):  # noqa: F811
     assert len(pdf_paths) == 1
 
 
-def test_find_files_with_extensions_str(generate_file_tree):  # noqa: F811
+def test_find_files_with_extensions_str(generate_file_tree):
     pdf_paths = find_files_with_extensions(
         search_dir=str(TEMP_DATA_DIR), extensions=['.pdf']
     )

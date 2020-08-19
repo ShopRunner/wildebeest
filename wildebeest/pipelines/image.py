0eb8106c4a03360008c4306062f3b899f5b0527b
gsganden@gmail.com <gsganden@gmail.com>
2020-08-19 10:02:42 -0500

    Replace class instance with child class

--- wildebeest/pipelines/image.py
+++ wildebeest/pipelines/image.py
@@ -1,10 +1,25 @@
 """Image-processing pipelines"""
+from typing import Any, Callable, Iterable, Optional, Union
+
 from wildebeest import Pipeline
 from wildebeest.load_funcs.image import load_image_from_url
 from wildebeest.write_funcs.image import write_image
 
 
-download_image_pipeline = Pipeline(
-    load_func=load_image_from_url, ops=[], write_func=write_image
-)
-"""Basic pipeline for downloading images"""
+class DownloadImagePipeline(Pipeline):
+    """
+    Class for defining a pipeline that downloads images.
+
+    Attributes
+    ----------
+    ops
+        See `wildebeest.pipelines.Pipeline`.
+    """
+
+    def __init__(
+        self,
+        ops: Optional[
+            Union[Callable[[Any], Any], Iterable[Callable[[Any], Any]]]
+        ] = None,
+    ):
+        super().__init__(load_func=load_image_from_url, ops=ops, write_func=write_image)
